7720a1f4e73fe171cf2d78373b9fad516436ae28
Ivy Mahncke <imahncke@olin.edu>
2024-11-04 11:06:45 -0500

    latest

--- machine_vision/contour_detection.py
+++ machine_vision/contour_detection.py
@@ -1,27 +1,36 @@
 # pylint: disable=all
 import cv2
+import numpy
 
 """
 Contour detection and drawing using different extraction modes to complement 
 the understanding of hierarchies
 """
+ERR_THRESH = 1
 
-image2 = cv2.imread("image_1.jpg")
-image2 = cv2.convertScaleAbs(image2, alpha=5.0, beta=0)
-img_gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
-ret, thresh2 = cv2.threshold(img_gray2, 150, 255, cv2.THRESH_BINARY)
+image = cv2.imread("image_1.jpg")
+img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
 
-contours4, hierarchy4 = cv2.findContours(
-    thresh2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+contours, hierarchy = cv2.findContours(
+    thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
 )
-image_copy5 = image2.copy()
-for i, contour in enumerate(contours4):
-    print(f"{i} weiner goofy haha silly cum devil")
-    cv2.drawContours(image_copy5, [contour], 0, (0, 255, 0), 2, cv2.LINE_AA)
+image_copy = image.copy()
+for i, contour_primary in enumerate(contours):
+    print(f"Contour {i}: {contour_primary}")
+    segment_primary = (contour_primary[0][0], contour_primary[-1][0])
+    for j, contour_sec in enumerate(contours):
+        segment_secondary = (contour_sec[0][0], contour_sec[-1][0])
+        # compare x
+        for k in range(0,4):
+            error = numpy.subtract(segment_primary[k % 2], segment_secondary[(k + 1) % 2])
+            if error[0] < ERR_THRESH and error[1] < ERR_THRESH:
 
+    
+cv2.drawContours(image_copy, contours, 0, (0, 20*i, 0), 2, cv2.LINE_AA)
 # see the results
-cv2.imshow("EXTERNAL", image_copy5)
-print(f"EXTERNAL: {hierarchy4}")
+cv2.imshow("EXTERNAL", image_copy)
+print(f"EXTERNAL: {hierarchy}")
 cv2.waitKey(0)
-cv2.imwrite("contours_retr_external.jpg", image_copy5)
+cv2.imwrite("contours_retr_external.jpg", image_copy)
 cv2.destroyAllWindows()
--- machine_vision/machine_vision.py
+++ machine_vision/machine_vision.py
@@ -1,5 +1,20 @@
 def main():
     print('Hi from machine_vision.')
+    # get an image
+        # draw contours around each object in the image
+        # for each contour, see if endpoint matches with another contour
+            # if so, merge them into one contour
+            # this should result in sealed boxes
+        # use dimensions of contour to group internal keypoints
+
+    # get a second image
+        # note a given translation between the two images (eg +50px X)
+        # repeat all the prior steps for the second image
+        # apply the given translation to each original keypoint (predicted keypoints)
+        # for each predicted keypoint, find the closest keypoint in the new image
+            # label it with the same contour/object name
+
+    # display both images with superimposed and labeled contours/keypoint groups
 
 
 if __name__ == '__main__':
