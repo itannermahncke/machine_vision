b8ce2b87e72bf5004f1ef27d0edd7c308f22ff7d
Ivy Mahncke <imahncke@olin.edu>
2024-11-04 19:38:55 -0500

    bounding rects

--- machine_vision/contour_detection.py
+++ machine_vision/contour_detection.py
@@ -1,36 +1,33 @@
 # pylint: disable=all
 import cv2
-import numpy
+import numpy as np
 
 """
 Contour detection and drawing using different extraction modes to complement 
 the understanding of hierarchies
 """
-ERR_THRESH = 1
 
 image = cv2.imread("image_1.jpg")
 img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
 
 contours, hierarchy = cv2.findContours(
-    thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
 )
 image_copy = image.copy()
-for i, contour_primary in enumerate(contours):
-    print(f"Contour {i}: {contour_primary}")
-    segment_primary = (contour_primary[0][0], contour_primary[-1][0])
-    for j, contour_sec in enumerate(contours):
-        segment_secondary = (contour_sec[0][0], contour_sec[-1][0])
-        # compare x
-        for k in range(0,4):
-            error = numpy.subtract(segment_primary[k % 2], segment_secondary[(k + 1) % 2])
-            if error[0] < ERR_THRESH and error[1] < ERR_THRESH:
+for i, cnt in enumerate(contours):
+    rect = cv2.minAreaRect(cnt)
+    area = cv2.contourArea(cnt)
+    # remove rects that are too small
+    if area > 1000:
+        print(f"Rect: {rect}\n   Width: {rect[1][0]} Height: {rect[1][1]}\n   Area: {area}")
+        box = cv2.boxPoints(rect)
+        box = np.intp(box)
+        cv2.drawContours(image_copy,[box],0,(0,0,255),2, -1)
 
     
-cv2.drawContours(image_copy, contours, 0, (0, 20*i, 0), 2, cv2.LINE_AA)
+# cv2.drawContours(image_copy, contours, 0, (0, 20*i, 0), 2, cv2.LINE_AA)
 # see the results
 cv2.imshow("EXTERNAL", image_copy)
-print(f"EXTERNAL: {hierarchy}")
 cv2.waitKey(0)
-cv2.imwrite("contours_retr_external.jpg", image_copy)
 cv2.destroyAllWindows()
