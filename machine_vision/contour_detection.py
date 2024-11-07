6e62b75701dd68815b3bdf275a1d5c41b74e0af6
Ivy Mahncke <imahncke@olin.edu>
2024-11-07 11:42:56 -0500

    latest contour

--- machine_vision/contour_detection.py
+++ machine_vision/contour_detection.py
@@ -6,27 +6,33 @@ import numpy as np
 Contour detection and drawing using different extraction modes to complement 
 the understanding of hierarchies
 """
+SIZE_THRESH = 10
 
-image = cv2.imread("image_1.jpg")
+# get contours
+image = cv2.imread("images/can2.jpg")
+image = cv2.resize(image, (504 * 2, 378 * 2))
+image = cv2.medianBlur(image, 5)
+
 img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
-
-contours, hierarchy = cv2.findContours(
-    thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+ret, thresh = cv2.threshold(img_gray, 20, 255, cv2.THRESH_BINARY)
+th3 = cv2.adaptiveThreshold(
+    img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
 )
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 image_copy = image.copy()
+
+# create and draw bounding boxes
+bounding_boxes = []  # tuple of contour, rect, box
 for i, cnt in enumerate(contours):
     rect = cv2.minAreaRect(cnt)
     area = cv2.contourArea(cnt)
     # remove rects that are too small
-    if area > 1000:
-        print(f"Rect: {rect}\n   Width: {rect[1][0]} Height: {rect[1][1]}\n   Area: {area}")
+    if area > SIZE_THRESH:
         box = cv2.boxPoints(rect)
         box = np.intp(box)
-        cv2.drawContours(image_copy,[box],0,(0,0,255),2, -1)
+        bounding_boxes.append((cnt, rect, box))
+        cv2.drawContours(image_copy, [box], 0, (0, 0, 255), 2, -1)
 
-    
-# cv2.drawContours(image_copy, contours, 0, (0, 20*i, 0), 2, cv2.LINE_AA)
 # see the results
 cv2.imshow("EXTERNAL", image_copy)
 cv2.waitKey(0)
