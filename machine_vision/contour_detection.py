9779ca6bb703e01e5a55ec6b33582c444392ec81
Ivy Mahncke <imahncke@olin.edu>
2024-11-10 12:39:06 -0500

    completed for demo

--- machine_vision/contour_detection.py
+++ machine_vision/contour_detection.py
@@ -6,18 +6,16 @@ import numpy as np
 Contour detection and drawing using different extraction modes to complement 
 the understanding of hierarchies
 """
-SIZE_THRESH = 10
+SIZE_THRESH = 1000
+MAX_THRESH = 100000
 
 # get contours
-image = cv2.imread("images/can2.jpg")
+image = cv2.imread("/home/ivymahncke/ros2_ws/src/machine_vision/images/clean_obj_1.JPG")
 image = cv2.resize(image, (504 * 2, 378 * 2))
 image = cv2.medianBlur(image, 5)
 
 img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(img_gray, 20, 255, cv2.THRESH_BINARY)
-th3 = cv2.adaptiveThreshold(
-    img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
-)
+ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 image_copy = image.copy()
 
@@ -27,7 +25,7 @@ for i, cnt in enumerate(contours):
     rect = cv2.minAreaRect(cnt)
     area = cv2.contourArea(cnt)
     # remove rects that are too small
-    if area > SIZE_THRESH:
+    if SIZE_THRESH < area < MAX_THRESH:
         box = cv2.boxPoints(rect)
         box = np.intp(box)
         bounding_boxes.append((cnt, rect, box))
