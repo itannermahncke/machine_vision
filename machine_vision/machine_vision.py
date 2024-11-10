47be515a64589e485674fee7b8766847f765b563
cmawn <cmawn@olin.edu>
2024-11-09 22:02:31 -0500

    combined the contour detection and keypoints, still needs bug fixing

--- machine_vision/machine_vision.py
+++ machine_vision/machine_vision.py
@@ -16,9 +16,11 @@ class KeypointMatch(object):
         self.keypoint_algorithm = cv2.SIFT_create()
         self.matcher = cv2.BFMatcher()
         self.im = None
+        self.size_thresh = 100
+        self.size_tresh_max = 100000
 
-        self.corner_threshold = 0.0
-        self.ratio_threshold = 1.0
+        self.corner_threshold = 0.02
+        self.ratio_threshold = 0.75
 
         self.img1 = im1_filepath
         self.img2 = im2_filepath
@@ -32,13 +34,61 @@ class KeypointMatch(object):
         im1 = cv2.imread(self.img1)
         im2 = cv2.imread(self.img2)
 
+        im1 = cv2.resize(im1, (378 * 2, 504 * 2))
+        im2 = cv2.resize(im2, (378 * 2, 504 * 2))
+
         # Converts images to grayscale
         im1_bw = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
         im2_bw = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
 
-        # Detects and computes the keypoints for eahc image
+        # Detects and computes the keypoints for each image
         kp1, des1 = self.keypoint_algorithm.detectAndCompute(im1_bw, None)
         kp2, des2 = self.keypoint_algorithm.detectAndCompute(im2_bw, None)
+
+        # Find contours in each image
+        im1_bw = cv2.medianBlur(im1_bw, 5)
+        im2_bw = cv2.medianBlur(im2_bw, 5)
+        ret1, thresh1 = cv2.threshold(im1_bw, 20, 255, cv2.THRESH_BINARY)
+        # ret2, thresh2 = cv2.threshold(im2_bw, 20, 255, cv2.THRESH_BINARY)
+        contours1, hierarchy1 = cv2.findContours(
+            thresh1, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE
+        )
+        # contours2, hierarchy2 = cv2.findContours(
+        #     thresh2, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE
+        # )
+        im1_cpy = im1.copy()
+        im2_cpy = im2.copy()
+
+        # create bounding boxes for each group of contours
+        b_box1 = []
+        # b_box2 = []
+        filtered_contours = []
+        for i, cnt in enumerate(contours1):
+            rect1 = cv2.minAreaRect(cnt)
+            area1 = cv2.contourArea(cnt)
+            # remove rects that are too small
+            if area1 > self.size_thresh and area1 < self.size_tresh_max:
+                filtered_contours.append(cnt)
+                box = cv2.boxPoints(rect1)
+                box = np.intp(box)
+                b_box1.append((cnt, rect1, box))
+                cv2.drawContours(im1_cpy, [box], 0, (0, 0, 255), 2, -1)
+        # for i, cnt in enumerate(contours2):
+        #     rect2 = cv2.minAreaRect(cnt)
+        #     area2 = cv2.contourArea(cnt)
+        #     # remove rects that are too small
+        #     if area2 > self.size_thresh:
+        #         box = cv2.boxPoints(rect2)
+        #         box = np.intp(box)
+        #         b_box2.append((cnt, rect2, box))
+        #         cv2.drawContours(im2_cpy, [box], 0, (0, 0, 255), 2, -1)
+
+        # filters out keypoints that dont fall within a bounding box
+
+        # for i, kp in enumerate(kp2):
+        #     for cnt in contours2:
+        #         if cv2.pointPolygonTest(cnt, (kp.pt[0], kp.pt[1]), False):
+        #             kp_bbox2.append(kp)
 
         # Finds the matches of keypoints across images
         matches = self.matcher.knnMatch(des1, des2, k=2)
@@ -54,16 +104,37 @@ class KeypointMatch(object):
             ):
                 good_matches.append((m.queryIdx, m.trainIdx))
 
+        matches_1 = []
+        matches_2 = []
+        for pair in good_matches:
+            matches_1.append(pair[0])
+            matches_2.append(pair[1])
+
+        kp_inx = [-1] * len(good_matches)
+        print(good_matches)
+        # kp_bbox2 = []
+        for j, cnt in enumerate(filtered_contours):
+            for i, kp in enumerate(kp1):
+                if i in matches_1:
+                    if cv2.pointPolygonTest(cnt, (kp.pt[0], kp.pt[1]), False) >= 0:
+                        kp_inx[i] = j
+                        print(f"i:{i} j:{j} x:{kp[0]} y:{kp[1]}")
+                    else:
+                        print("failed ppt")
+
+        print(kp_inx)
+
         # creates new points using the good matches
         pts1 = np.zeros((len(good_matches), 2))
         pts2 = np.zeros((len(good_matches), 2))
         for idx in range(len(good_matches)):
+            # if kp_inx[idx] != -1:
             match = good_matches[idx]
             pts1[idx, :] = kp1[match[0]].pt
             pts2[idx, :] = kp2[match[1]].pt
 
         # creates new image to contain all the points
-        self.im = np.array(np.hstack((im1, im2)))
+        self.im = np.array(np.hstack((im1_cpy, im2)))
 
         # plots the points
         for i in range(pts1.shape[0]):
@@ -100,6 +171,12 @@ def main():
     # label it with the same contour/object name
 
     # display both images with superimposed and labeled contours/keypoint groups
+
+    matching = KeypointMatch("images/simple_1.jpg", "images/simple_2.jpg")
+    matching.compute_matches()
+    cv2.imshow("EXTERNAL", matching.im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
